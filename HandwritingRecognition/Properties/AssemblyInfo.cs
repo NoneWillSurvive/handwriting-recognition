5e4c033a41923716a1065ce55486b5e8afd1d0a6
Богданов Кирилл <bogdanovka@data-platform.ru>
2022-05-29 17:16:41 +0300

    fix

--- HandwritingRecognition/Properties/AssemblyInfo.cs
+++ HandwritingRecognition/Properties/AssemblyInfo.cs
@@ -1,15 +1,4 @@
-﻿/*
-Handwriting Recognition with WPF and Microsoft.Ink
-
-Original publication:
-https://www.codeproject.com/Articles/5282936/Handwriting-Recognition-WPF-and-Microsoft-Ink
-
-    Copyright © 2020 by Sergey A Kryukov
-    http://www.codeproject.com/Members/SAKryukov
-    http://www.SAKryukov.org
-*/
-
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("SAHand")]
